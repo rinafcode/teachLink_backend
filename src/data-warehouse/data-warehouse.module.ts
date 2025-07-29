e9a992c8758e8e1208c69ac0be3f2a4006557238
kamaldeen <kamalaliyu503@gmail.com>
2025-07-29 17:10:01 +0100

    Implemtented advanced data ware house

---
+++ src/data-warehouse/data-warehouse.module.ts
@@ -0,0 +1,24 @@
+import { Module } from '@nestjs/common';
+import { ETLPipelineService } from './etl/etl-pipeline.service';
+import { DimensionalModelingService } from './modeling/dimensional-modeling.service';
+import { DataQualityService } from './quality/data-quality.service';
+import { DataLineageService } from './lineage/data-lineage.service';
+import { IncrementalLoaderService } from './loading/incremental-loader.service';
+
+@Module({
+  providers: [
+    ETLPipelineService,
+    DimensionalModelingService,
+    DataQualityService,
+    DataLineageService,
+    IncrementalLoaderService,
+  ],
+  exports: [
+    ETLPipelineService,
+    DimensionalModelingService,
+    DataQualityService,
+    DataLineageService,
+    IncrementalLoaderService,
+  ],
+})
+export class DataWarehouseModule {} 
