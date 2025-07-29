e903360d92311fc96c417551b1b9f9488ebaa0ac
kamaldeen <kamalaliyu503@gmail.com>
2025-07-29 18:23:23 +0100

    service discovery

--- src/orchestration/discovery/service-discovery.service.ts
+++ src/orchestration/discovery/service-discovery.service.ts
@@ -4,15 +4,15 @@ import { Injectable } from '@nestjs/common';
 export class ServiceDiscoveryService {
   private registry: Map<string, string> = new Map();
 
-  // Register a service
+  // Registered a service
   async registerService(name: string, address: string): Promise<void> {
-    // TODO: Implement service registration
+    // TODO: Implement service registrations
     this.registry.set(name, address);
   }
 
-  // Discover a service
+  // Discovered a service
   async discoverService(name: string): Promise<string | undefined> {
-    // TODO: Implement service discovery
+    // TODO: Implement service discoverys
     return this.registry.get(name);
   }
 } 
