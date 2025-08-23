47c21f4b573ab7523e8413d4ff292244e5a8975a
Yusuf Falade <faladeyusuf54@gmail.com>
2025-08-23 14:06:34 +0100

    create and response dtos

---
+++ src/certificate/dto/create-certificate.dto.ts
@@ -0,0 +1,65 @@
+import {
+  IsString,
+  IsEmail,
+  IsUUID,
+  IsEnum,
+  IsOptional,
+  IsArray,
+  IsObject,
+  IsDateString,
+  MaxLength,
+  MinLength,
+} from "class-validator"
+import { CertificateType } from "../entities/certificate.entity"
+
+export class CreateCertificateDto {
+  @IsString()
+  @MinLength(3)
+  @MaxLength(255)
+  title: string
+
+  @IsOptional()
+  @IsString()
+  @MaxLength(1000)
+  description?: string
+
+  @IsEnum(CertificateType)
+  type: CertificateType
+
+  @IsUUID()
+  recipientId: string
+
+  @IsString()
+  @MinLength(2)
+  @MaxLength(255)
+  recipientName: string
+
+  @IsEmail()
+  recipientEmail: string
+
+  @IsUUID()
+  issuerId: string
+
+  @IsString()
+  @MinLength(2)
+  @MaxLength(255)
+  issuerName: string
+
+  @IsString()
+  @MinLength(2)
+  @MaxLength(255)
+  issuerOrganization: string
+
+  @IsOptional()
+  @IsArray()
+  @IsString({ each: true })
+  skills?: string[]
+
+  @IsOptional()
+  @IsObject()
+  metadata?: Record<string, any>
+
+  @IsOptional()
+  @IsDateString()
+  expiresAt?: string
+}
