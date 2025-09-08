6fb4d79eda69b13d3c80b50efa43dc511cf5effe
KAMALDEEN333 <kamalaliyu503@gmail.com>
2025-09-08 11:44:21 +0100

    created collarboration module

---
+++ src/collaboration/collaboration.module.ts
@@ -0,0 +1,28 @@
+import { Module } from '@nestjs/common';
+import { CollaborationService } from './collaboration.service';
+import { SharedDocumentService } from './documents/shared-document.service';
+import { WhiteboardService } from './whiteboard/whiteboard.service';
+import { VersionControlService } from './versioning/version-control.service';
+import { CollaborationPermissionsService } from './permissions/collaboration-permissions.service';
+
+/**
+ * Module for real-time collaboration features including shared documents,
+ * whiteboards, version control, and permission management
+ */
+@Module({
+  providers: [
+    CollaborationService,
+    SharedDocumentService,
+    WhiteboardService,
+    VersionControlService,
+    CollaborationPermissionsService,
+  ],
+  exports: [
+    CollaborationService,
+    SharedDocumentService,
+    WhiteboardService,
+    VersionControlService,
+    CollaborationPermissionsService,
+  ],
+})
+export class CollaborationModule {}
