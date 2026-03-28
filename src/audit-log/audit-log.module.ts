09bbd809c53efa75681e4e03ed0ea89e9c736937
ahmadogo <abdulkareemahmad413@gmail.com>
2026-03-28 08:29:29 +0100

    Implement Bulk Operations API

--- src/audit-log/audit-log.module.ts
+++ src/audit-log/audit-log.module.ts
@@ -1,20 +1,24 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
+import { ScheduleModule } from '@nestjs/schedule';
 import { AuditLog } from './audit-log.entity';
 import { AuditLogService } from './audit-log.service';
 import { AuditLogController } from './audit-log.controller';
 import { AuditLogInterceptor } from './interceptors/audit-log.interceptor';
+import { AuditRetentionTask } from './tasks/audit-retention.task';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([AuditLog]),
     ConfigModule,
+    ScheduleModule.forRoot(),
   ],
   controllers: [AuditLogController],
   providers: [
     AuditLogService,
     AuditLogInterceptor,
+    AuditRetentionTask,
   ],
   exports: [
     AuditLogService,
