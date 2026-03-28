8dee40462f54c0abd10c8ea1fe47a9a9a62e69ca
ahmadogo <abdulkareemahmad413@gmail.com>
2026-03-28 08:15:10 +0100

    Implement File Upload Validation

--- src/media/media.module.ts
+++ src/media/media.module.ts
@@ -1,11 +1,16 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { BullModule } from '@nestjs/bull';
+import { RedisModule } from '@nestjs-modules/ioredis';
 import { MediaController } from './media.controller';
 import { MediaService } from './media.service';
 import { FileStorageService } from './storage/file-storage.service';
 import { VideoProcessingService } from './processing/video-processing.service';
 import { DocumentProcessingService } from './processing/document-processing.service';
+import { ImageProcessingService } from './processing/image-processing.service';
+import { FileValidationService } from './validation/file-validation.service';
+import { MalwareScanningService } from './validation/malware-scanning.service';
+import { UploadProgressService } from './validation/upload-progress.service';
 import { ContentMetadata } from '../cdn/entities/content-metadata.entity';
 import { VideoProcessor } from './processing/video.processor';
 
@@ -13,6 +18,7 @@ import { VideoProcessor } from './processing/video.processor';
   imports: [
     TypeOrmModule.forFeature([ContentMetadata]),
     BullModule.registerQueue({ name: 'media-processing' }),
+    RedisModule,
   ],
   controllers: [MediaController],
   providers: [
@@ -20,9 +26,19 @@ import { VideoProcessor } from './processing/video.processor';
     FileStorageService,
     VideoProcessingService,
     DocumentProcessingService,
+    ImageProcessingService,
+    FileValidationService,
+    MalwareScanningService,
+    UploadProgressService,
     // processors
     VideoProcessor,
   ],
-  exports: [MediaService, FileStorageService, VideoProcessingService],
+  exports: [
+    MediaService,
+    FileStorageService,
+    VideoProcessingService,
+    FileValidationService,
+    ImageProcessingService,
+  ],
 })
 export class MediaModule {}
