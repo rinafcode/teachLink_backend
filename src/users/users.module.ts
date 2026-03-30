9bf0f4881e2c1b961666f2ff3acedbc5c99dfba2
Ahmed <ahmedmahmud1994@hotmail.com>
2026-03-30 11:54:58 +0100

    feat(users): add queued user data export with history

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,15 +1,31 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { BullModule } from '@nestjs/bull';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { User } from './entities/user.entity';
+import { Enrollment } from '../courses/entities/enrollment.entity';
 import { RolesGuard } from '../auth/guards/roles.guard';
 import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
+import {
+  ExportService,
+  UserDataExportProcessor,
+  UserExportHistory,
+} from '../common/export/export.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [
+    TypeOrmModule.forFeature([User, Enrollment, UserExportHistory]),
+    BullModule.registerQueue({ name: 'user-data-export' }),
+  ],
   controllers: [UsersController],
-  providers: [UsersService, RolesGuard, JwtAuthGuard],
+  providers: [
+    UsersService,
+    ExportService,
+    UserDataExportProcessor,
+    RolesGuard,
+    JwtAuthGuard,
+  ],
   exports: [UsersService],
 })
 export class UsersModule {}
