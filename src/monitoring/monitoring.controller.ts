95e5a4971bd878dcd667f7d02b7286d2798660c4
Ahmed <ahmedmahmud1994@hotmail.com>
2026-03-30 11:13:11 +0100

    feat(monitoring): add scheduled task tracking and alerts

--- src/monitoring/monitoring.controller.ts
+++ src/monitoring/monitoring.controller.ts
@@ -1,15 +1,24 @@
 import { Controller, Get, Res } from '@nestjs/common';
 import { MetricsCollectionService } from './metrics/metrics-collection.service';
 import { Response } from 'express';
+import { ScheduledTaskMonitoringService } from './scheduled-task-monitoring.service';
 
 @Controller('metrics')
 export class MonitoringController {
-  constructor(private readonly metricsService: MetricsCollectionService) {}
+  constructor(
+    private readonly metricsService: MetricsCollectionService,
+    private readonly scheduledTaskMonitoringService: ScheduledTaskMonitoringService,
+  ) {}
 
   @Get()
   async getMetrics(@Res() res: Response) {
     const metrics = await this.metricsService.getMetrics();
     res.set('Content-Type', this.metricsService.getRegistry().contentType);
     res.send(metrics);
+  }
+
+  @Get('scheduled-tasks/dashboard')
+  getScheduledTasksDashboard() {
+    return this.scheduledTaskMonitoringService.getDashboard();
   }
 }
