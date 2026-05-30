d244b9cdb44384c1aa9fee0d1dd2b759ca2aa9ba
Rukayat Zakariyau <Rukme4k4@gmail.com>
2026-05-30 20:07:07 +0100

    Revert "Feat/segment analytics"

--- src/analytics/analytics.module.ts
+++ src/analytics/analytics.module.ts
@@ -5,16 +5,15 @@ import { AnalyticsController } from './analytics.controller';
 import { FingerprintModule } from './fingerprint/fingerprint.module';
 import { FingerprintInterceptor } from './fingerprint/fingerprint.interceptor';
 import { MetricsCollectionService } from '../monitoring/metrics/metrics-collection.service';
-import { SegmentModule } from './segment/segment.module';
 
 @Module({
-  imports: [FingerprintModule, SegmentModule],
+  imports: [FingerprintModule],
   providers: [
     MetricsCollectionService,
     AnalyticsService,
     { provide: APP_INTERCEPTOR, useClass: FingerprintInterceptor },
   ],
   controllers: [AnalyticsController],
-  exports: [AnalyticsService, FingerprintModule, SegmentModule],
+  exports: [AnalyticsService, FingerprintModule],
 })
 export class AnalyticsModule {}
