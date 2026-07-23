f75a5848bd5d6ab9de0f6d7aaab4e3d7ecaca410
lamborghini21 <clockmek@gmail.com>
2026-07-24 00:17:38 +0100

    feat(payments): add daily reconciliation job for unmatched transactions (#856)

--- src/payments/payments.module.ts
+++ src/payments/payments.module.ts
@@ -1,17 +1,26 @@
 import { Module } from '@nestjs/common';
+import { HttpModule } from '@nestjs/axios';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CurrencyModule } from '../currency/currency.module';
+import { AuditLogModule } from '../audit-log/audit-log.module';
 import { Payment } from './entities/payment.entity';
 import { Subscription } from './entities/subscription.entity';
 import { Invoice } from './entities/invoice.entity';
 import { Refund } from './entities/refund.entity';
 import { PricingService } from './services/pricing.service';
 import { PricingController } from './controllers/pricing.controller';
+import { PaymentReconciliationJob } from './reconciliation/reconciliation.service';
+import { PaymentReconciliationController } from './reconciliation/reconciliation.controller';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Payment, Subscription, Invoice, Refund]), CurrencyModule],
-  providers: [PricingService],
-  controllers: [PricingController],
-  exports: [PricingService, CurrencyModule],
+  imports: [
+    TypeOrmModule.forFeature([Payment, Subscription, Invoice, Refund]),
+    CurrencyModule,
+    AuditLogModule,
+    HttpModule,
+  ],
+  providers: [PricingService, PaymentReconciliationJob],
+  controllers: [PricingController, PaymentReconciliationController],
+  exports: [PricingService, CurrencyModule, PaymentReconciliationJob],
 })
 export class PaymentsModule {}
