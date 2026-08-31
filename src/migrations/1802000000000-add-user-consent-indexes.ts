35ec6cebba1946b0b235077da67e2ab084ca0986
Peolite001 <peolite001@gmail.com>
2026-08-31 11:56:22 +0100

    fix: lint errors in migration file

--- src/migrations/1802000000000-add-user-consent-indexes.ts
+++ src/migrations/1802000000000-add-user-consent-indexes.ts
@@ -4,12 +4,20 @@ export class AddUserConsentIndexes1802000000000 implements MigrationInterface {
   name = 'AddUserConsentIndexes1802000000000';
 
   public async up(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(`CREATE INDEX "IDX_user_consents_userId" ON "user_consents" ("userId")`);
-    await queryRunner.query(`CREATE INDEX "IDX_user_consents_consentType" ON "user_consents" ("consentType")`);
+    await queryRunner.query(
+      'CREATE INDEX "IDX_user_consents_userId" ON "user_consents" ("userId")',
+    );
+    await queryRunner.query(
+      'CREATE INDEX "IDX_user_consents_consentType" ON "user_consents" ("consentType")',
+    );
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(`DROP INDEX "IDX_user_consents_consentType"`);
-    await queryRunner.query(`DROP INDEX "IDX_user_consents_userId"`);
+    await queryRunner.query(
+      'DROP INDEX "IDX_user_consents_consentType"',
+    );
+    await queryRunner.query(
+      'DROP INDEX "IDX_user_consents_userId"',
+    );
   }
 }
